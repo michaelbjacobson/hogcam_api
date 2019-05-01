648d4243ebc8d412ad2ec13f9214bcc8c76c9cba
Michael Jacobson <contact@michaeljacobson.co.uk>
2019-05-01 15:02:24 +0100

    commit

--- dependencies.rb
+++ dependencies.rb
@@ -3,6 +3,10 @@ require 'dotenv/load'
 require 'sinatra/base'
 require 'thin'
 
+# Native modules
+require 'json'
+require 'logger'
+
 # Project files
 require 'lib/raspberry_pi'
 require 'lib/mock_pi'
